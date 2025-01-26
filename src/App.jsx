4240d847ebe814b71ba167d2d5b9af7827ad8b00
Orion <mendozagarcia@uninorte.edu.co>
2025-01-25 22:52:36 -0500

    Tutorials, scrollbar-x

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import './App.css'
 import {Start} from'./pages/start/start.jsx'
 import { Home } from './pages/home/home.jsx'
 import { AboutUs } from './pages/aboutUs/aboutUs.jsx'
+import { Tutorials } from './pages/tutorials/tutorials.jsx'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Route path="/" element={<Start />} />
       <Route path="/Home" element={<Home/>} />
       <Route path="/AboutUs" element={<AboutUs/>} />
+      <Route path="/Tutorials" element={<Tutorials/>} />
     </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/tutorials/tutorials.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './style.css'
+import { NavBar } from '../../components/NavBar/navBar.jsx'
+import { Banner } from '../../components/Banner/banner.jsx'
+import { Card } from '../../components/Card/card.jsx'
+
+export const Tutorials = () => {
+    return (
+        <div className='Tutorials'>
+            <NavBar />
+            <Banner>
+                <Card>
+                    <h2>Tutoriales</h2>
+                    <div className="infoTutorial">
+                        <div className="General">
+                            <h3>General</h3>
+                            <div className="viewList">
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                
+                            </div>
+                        </div>
+                        <div className="Streaming">
+                            <h3>Streaming</h3>
+                            <div className="viewList"><a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                <a href="https://drive.google.com/file/d/1aNLgrHxGl3zRf5p74wwAceyUE7ORY-Wv/view?usp=sharing"><img src="src\assets\imgs\Enhypen.png" alt="DuckAd" /></a>
+                                </div>
+                        </div>
+                        <div className="Comeback">
+                            <h3>Comeback</h3>
+                            <div className="viewList"><a href=""><img src="" alt="" /></a></div>
+                        </div>
+                    </div>
+                </Card>
+            </Banner>
+        </div>
+    )
+}
