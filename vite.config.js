79dbe2a95488842387a8387f6d0ac1c36408423d
Orion <mendozagarcia@uninorte.edu.co>
2025-03-31 23:00:55 -0500

    2nd try website hosting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // Asegúrate de que la salida de los archivos sea en la carpeta 'build'
+  },
 })
